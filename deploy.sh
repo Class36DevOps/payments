c30eed666c2ba4287d65c7b302a999287d6647b5
Jsima <jsimabekhai@gmail.com>
2024-05-20 16:58:59 +0000

    Testing file added

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@ deployment must be reviewed
 deployment must be reviewed and commited
 deployment must be reviewed by all managers before commit
 this is good for everyone 
+
+Application is ready for update and review
