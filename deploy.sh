6d1145dc951e4ea7918c7c21373d3f10a6665326
Raymond <rimabekhai@gmail.com>
2024-05-20 13:25:46 +0000

    deployed updated

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ app is ready to be deployed
 deployment must be reviewed
 deployment must be reviewed and commited
 deployment must be reviewed by all managers before commit
+this is good for everyone 
